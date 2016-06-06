daeeaa9675ea50644b52dde6596dee40c59b8ebc
Igor K <feling87@gmail.com>
2016-06-06 18:36:05 +0300

    Validation

--- dev/shared/recipe.ts
+++ dev/shared/recipe.ts
@@ -3,12 +3,12 @@ export class Recipe {
     name:string;
     content:string;
     imageUrl:string;
-    ingredient:Ingredient[];
+    ingredients:Ingredient[];
 
     constructor(name:string, content:string, imageUrl:string, ingredients:Ingredient[]) {
         this.name = name;
         this.content = content;
         this.imageUrl = imageUrl;
-        this.ingredient = ingredients;
+        this.ingredients = ingredients;
     }
 }
